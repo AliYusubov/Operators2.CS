4d1ec703e090cd8c75ee123900cfea48ae5a2e5c
AliYusubov <120775751+AliYusubov@users.noreply.github.com>
2023-07-20 01:36:16 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+console_app.csharp
+
+7. Operators 2 Exercise
+ ![Operators 2 ex](op2.png)
---
+++ operators2nd_ex.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Diagnostics.Metrics;
+
+namespace ALi
+{
+    class NumberManipulator
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int c = 7;
+            int d = 2;
+            int e = 4;
+
+            int result = a++ + --b * c - e-- + ++d / a + b-- * c / e;
+            Console.WriteLine("Initial values:");
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            Console.WriteLine($"c: {c}");
+            Console.WriteLine($"d: {d}");
+            Console.WriteLine($"e: {e}");
+
+            Console.WriteLine("\nExpression Result:");
+            Console.WriteLine($"Result = {result}");
+
+        }
+
+
+    }
+}
